50f08a53ebaa6b5ea295c56a4dcbd3a68c65a6f5
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-03-28 08:20:53 +0900

    イベントハンドラにおけるバッチ処理
    react17でもイベントハンドラ内でもバッチ処理がされていた

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 import React, { useEffect } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import AutoBatchEventHandler from './components/AutoBatchEventHandler'
 
 function App() {
-  console.log('Appレンダリング')
+  // console.log('Appレンダリング')
   useEffect(() => {
-    console.log('useEffect')
+    // console.log('useEffect')
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AutoBatchEventHandler />
     </div>
   )
 }
---
+++ src/components/AutoBatchEventHandler.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const AutoBatchEventHandler = () => {
+  const [state1, setState1] = useState<number>(0)
+  const [state2, setState2] = useState<number>(0)
+
+  const onClickUpdateButton = () => {
+    console.log(state1)
+    setState1((state1) => state1 + 1)
+    console.log(state1) // バッチ処理をしているため、値はまだ更新されていない
+    setState2((state2) => state2 + 1)
+  }
+  return (
+    <div>
+      <p>AutoBatchEventHandler確認用（イベントハンドラ）</p>
+      <button onClick={onClickUpdateButton}>State更新</button>
+      <p>State1: {state1}</p>
+      <p>State2: {state2}</p>
+    </div>
+  )
+}
+export default AutoBatchEventHandler
