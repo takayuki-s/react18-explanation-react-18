18dd016bb6561385aac2a835efad1b4dc5a949f9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-03-30 08:36:51 +0900

    Transition（startTransition）
    優先度の低い処理をstartTransitionで関数ラップしてあげる

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, startTransition } from 'react'
 import { Avatar } from './Avatar'
 
 type Task = {
@@ -44,7 +44,9 @@ export const Transition = () => {
 
   const onClickAssignee = (assignee: string) => {
     setSelectedAssignee(assignee)
-    setTaskList(filteringAssignee(assignee))
+    startTransition(() => {
+      setTaskList(filteringAssignee(assignee))
+    })
   }
   return (
     <div>
