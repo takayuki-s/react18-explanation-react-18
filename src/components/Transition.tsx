bf78f073b2b325e9971c92ef98212cb33cb7d2a9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-03-31 09:35:18 +0900

    Transition（useTransition）

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,4 +1,4 @@
-import { useState, startTransition } from 'react'
+import { useState, useTransition } from 'react'
 import { Avatar } from './Avatar'
 
 type Task = {
@@ -39,6 +39,7 @@ const filteringAssignee = (assignee: string) => {
 }
 
 export const Transition = () => {
+  const [isPending, startTransition] = useTransition()
   const [selectedAssignee, setSelectedAssignee] = useState<string>('')
   const [taskList, setTaskList] = useState<Task[]>(tasks)
 
@@ -76,7 +77,12 @@ export const Transition = () => {
       {taskList.map((task) => (
         <div
           key={task.id}
-          style={{ width: '300px', margin: 'auto', background: 'lavender' }}
+          style={{
+            width: '300px',
+            margin: 'auto',
+            background: 'lavender',
+            opacity: isPending ? 0.5 : 1,
+          }}
         >
           <p>タイトル: {task.title}</p>
           <p>担当者: {task.assignee}</p>
