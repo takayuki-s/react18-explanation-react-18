d3a0bb2dbe23ee46a882f4e0519efa7c8eb24c1a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-04-07 11:53:56 +0900

    アルバム一覧コンポーネント化

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -1,7 +1,34 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+type Album = {
+  userId: number
+  id: number
+  title: string
+}
+
+const fetchAlbums = async () => {
+  const result = await axios.get<Album[]>(
+    'https://jsonplaceholder.typicode.com/albums'
+  )
+  return result.data
+}
+
 export const AlbumList = () => {
+  const { data } = useQuery<Album[]>(['albums'], fetchAlbums)
   return (
-    <div>
+    <div
+      style={{
+        height: '300px',
+        border: '2px solid gray',
+        background: 'cornsilk',
+        overflowY: 'scroll',
+      }}
+    >
       <h2>アルバム</h2>
+      {data?.map((album) => (
+        <p key={album.id}>{album.title}</p>
+      ))}
     </div>
   )
 }
--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,30 +1,10 @@
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
 import { AlbumList } from './AlbumList'
 import { Sidebar } from './Sidebar'
 import { TodoList } from './TodoList'
 
-type Album = {
-  userId: number
-  id: number
-  title: string
-}
-
-const fetchAlbums = async () => {
-  const result = await axios.get<Album[]>(
-    'https://jsonplaceholder.typicode.com/albums'
-  )
-  return result.data
-}
-
 export const ReactQuery = () => {
-  const { data } = useQuery<Album[]>(['albums'], fetchAlbums)
   return (
     <div style={{ display: 'flex', padding: '16px' }}>
-      {/* <p>ReactQuery</p>
-      {data?.map((album) => (
-        <p key={album.id}>{album.title}</p>
-      ))} */}
       <Sidebar />
       <div style={{ flexGrow: '1' }}>
         <AlbumList />
