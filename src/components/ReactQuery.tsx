60f2a1952ea0bec5d6b92f2a3d16fbaed9b7bb76
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-04-10 06:08:19 +0900

    SuspenseとTransition
    データ取得中もTransitionで前のコンテンツを表示させておく

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from 'react'
+import { Suspense, useState, useTransition } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { AlbumList } from './AlbumList'
 import { Sidebar } from './Sidebar'
@@ -8,11 +8,13 @@ type Tabs = 'todo' | 'album'
 
 export const ReactQuery = () => {
   const [selectedTab, setSelectedTab] = useState<Tabs>('todo')
+  const [isPending, startTransition] = useTransition()
 
   const buttonStyle = {
     padding: '12px',
     fontSize: '16px',
     border: 'none',
+    opacity: isPending ? 0.5 : 1,
   }
   const albumButtonStyle = {
     ...buttonStyle,
@@ -26,7 +28,9 @@ export const ReactQuery = () => {
   }
 
   const onClickTabButton = (tab: Tabs) => {
-    setSelectedTab(tab)
+    startTransition(() => {
+      setSelectedTab(tab)
+    })
   }
   return (
     <div style={{ display: 'flex', padding: '16px' }}>
